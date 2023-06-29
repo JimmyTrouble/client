2827e2adabb45af4d65bb35ffb8ad5fcc96aaba6
JimmyTrouble <114571635+JimmyTrouble@users.noreply.github.com>
2023-06-29 18:46:40 +1000

    intial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api").then((response) =>
+      response.json().then((data) => {
+        setBackendData(data);
+      })
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {console.log(backendData)}
+      {typeof backendData.users === "undefined" ? (
+        <p>loading</p>
+      ) : (
+        backendData.users.map((item, i) => <p key={i}>{item}</p>)
+      )}
     </div>
   );
 }
